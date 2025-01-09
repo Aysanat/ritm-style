ed03b6b1af637b2340d4dcaef41ce1fd1de08d07
Aysanat <aysanat21aysanat@gmail.com>
2025-01-09 05:02:09 +0300

    blog(a little stylized), choose a map

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+  $('.js-signing-item').on('click', function(event) {
+    event.preventDefault();
 
+    $('.js-signing-item').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).index('.js-signing-item');
+
+    $('.js-signing-visual').removeClass('active');
+    $('.js-signing-visual').eq(index).addClass('active');
+
+  });
+})
